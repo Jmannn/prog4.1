2dd386ffe4f2062976c168137bbd6592f594d156
Johnathan Mann <jmann@oucs1383.otago.ac.nz>
2018-08-14 14:19:59 +1200

    Initial Commit

---
+++ prog4.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  prog4.1
+//
+//  Created by Johnathan Mann on 8/14/18.
+//  Copyright © 2018 Johnathan Mann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
